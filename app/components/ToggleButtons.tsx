a5b9f8c71b4240c2c63f5260fdc8b3f575125c82
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-04 09:04:14 +0330

    feat(togglebutton): up and down icons

--- app/components/ToggleButtons.tsx
+++ app/components/ToggleButtons.tsx
@@ -2,9 +2,9 @@
 import { useState } from "react";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
-import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
-import { Typography } from "@mui/material"
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import { Typography } from "@mui/material";
 
 export default function ToggleButtons() {
   const [alignment, setAlignment] = useState<string | null>("left");
@@ -23,21 +23,27 @@ export default function ToggleButtons() {
       onChange={handleAlignment}
       aria-label="timer"
       size="large"
-      sx={{backgroundColor:'white'}}
+      sx={{ backgroundColor: "white" }}
     >
-      <ToggleButton value="up" aria-label="increasment"  sx={{display:'flex', alignItems:'center', gap:'5px'}}>
-        <Typography variant="body1" component="p"  sx={{color:"black"}}>
+      <ToggleButton
+        value="up"
+        aria-label="increasment"
+        sx={{ display: "flex", alignItems: "center", gap: "5px" }}
+      >
+        <Typography variant="body1" component="p" sx={{ color: "black" }}>
           Increase
         </Typography>
-        <KeyboardDoubleArrowUpIcon sx={{color:"black"}}/>
-       
+        <KeyboardDoubleArrowUpIcon sx={{ color: "black" }} />
       </ToggleButton>
-      <ToggleButton value="down" aria-label="decreasment" sx={{display:'flex', alignItems:'center', gap:'5px'}}>
-       
-        <Typography variant="body1" component="p"  sx={{color:"black"}}>
+      <ToggleButton
+        value="down"
+        aria-label="decreasment"
+        sx={{ display: "flex", alignItems: "center", gap: "5px" }}
+      >
+        <Typography variant="body1" component="p" sx={{ color: "black" }}>
           Decrease
         </Typography>
-        <KeyboardDoubleArrowDownIcon  sx={{color:"black"}}/>
+        <KeyboardDoubleArrowDownIcon sx={{ color: "black" }} />
       </ToggleButton>
     </ToggleButtonGroup>
   );
