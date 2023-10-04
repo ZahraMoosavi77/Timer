f9caaf9958d8e050aae090efae79c01a9b2174c2
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-04 09:19:04 +0330

    feat(Button.tsx):  add icons

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,17 +1,31 @@
-'use client'
+"use client";
 
-import React from 'react'
-import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
-import StopCircleIcon from '@mui/icons-material/StopCircle';
-import { Box } from '@mui/material';
-import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import React from "react";
+import PlayCircleFilledWhiteIcon from "@mui/icons-material/PlayCircleFilledWhite";
+import StopCircleIcon from "@mui/icons-material/StopCircle";
+import { Box, Typography } from "@mui/material";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
 export default function Button() {
   return (
-    <Box>
-        <PlayCircleFilledWhiteIcon/>
-        <StopCircleIcon/>
-        <RestartAltIcon/>
-        
+    <Box display={"flex"} gap={5}>
+      <Box display={"flex"} flexDirection={"column"} gap={1}>
+        <PlayCircleFilledWhiteIcon
+          sx={{ color: "white", width: "36px", height: "36px" }}
+        />
+        <Typography color={"white"}>Start</Typography>
+      </Box>
+      <Box display={"flex"} flexDirection={"column"} gap={1}>
+        <StopCircleIcon
+          sx={{ color: "white", width: "36px", height: "36px" }}
+        />
+        <Typography color={"white"}>Stop</Typography>
+      </Box>
+      <Box display={"flex"} flexDirection={"column"} gap={1}>
+        <RestartAltIcon
+          sx={{ color: "white", width: "36px", height: "36px" }}
+        />
+        <Typography color={"white"}>Reset</Typography>
+      </Box>
     </Box>
-  )
+  );
 }
