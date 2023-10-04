8bb2220cb2981cf6a96518e9d6631f7c752f4605
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-04 09:03:44 +0330

    feat(togglebutton): up and down icons

--- app/components/ToggleButtons.tsx
+++ app/components/ToggleButtons.tsx
@@ -1,13 +1,10 @@
 "use client";
 import { useState } from "react";
-import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
-import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
-import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
-import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
-import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import { Typography } from "@mui/material"
 
 export default function ToggleButtons() {
   const [alignment, setAlignment] = useState<string | null>("left");
@@ -28,11 +25,19 @@ export default function ToggleButtons() {
       size="large"
       sx={{backgroundColor:'white'}}
     >
-      <ToggleButton value="up" aria-label="increasment">
-        <ArrowCircleUpIcon sx={{ color: "black" }} />
+      <ToggleButton value="up" aria-label="increasment"  sx={{display:'flex', alignItems:'center', gap:'5px'}}>
+        <Typography variant="body1" component="p"  sx={{color:"black"}}>
+          Increase
+        </Typography>
+        <KeyboardDoubleArrowUpIcon sx={{color:"black"}}/>
+       
       </ToggleButton>
-      <ToggleButton value="down" aria-label="decreasment">
-        <ArrowCircleDownIcon sx={{ color: "black" }} />
+      <ToggleButton value="down" aria-label="decreasment" sx={{display:'flex', alignItems:'center', gap:'5px'}}>
+       
+        <Typography variant="body1" component="p"  sx={{color:"black"}}>
+          Decrease
+        </Typography>
+        <KeyboardDoubleArrowDownIcon  sx={{color:"black"}}/>
       </ToggleButton>
     </ToggleButtonGroup>
   );
