43acd09191c5de7967ab3c4157bf02b397d915fc
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-06 15:46:00 +0330

    fix(number): responsive

--- app/components/Number.tsx
+++ app/components/Number.tsx
@@ -2,30 +2,31 @@
 
 import React from "react";
 import { Box, Typography } from "@mui/material";
-type NumberType = { date: string; isLast: boolean };
+import {useTimerContext} from "../../hooks/useTimerContext"
+type NumberType = { date: {name:string, value:number}; isLast: boolean };
 export default function number({ date, isLast }: NumberType) {
   return (
     <Box
       display={"flex"}
       flexDirection={"column"}
       justifyContent={"center"}
-    //   alignItems={"flex-start"}
-      sx={{alignItems:{xs:'center', sm:'flex-start'}}}
+      // alignItems={"flex-start"}
+      sx={{alignItems:{xs:'center', }, width:{xs:'55px',sm:'80px'}, textAlign:'center'}}
     >
       <Typography
-        // variant="h3"
-        component="p"
-        sx={{ color: "#fff" , gap:{md:3,xs:1}, fontFamily:{xs:'20px', md:'30px'}}}
+        variant="h3"
+        component="h3"
+        sx={{ color: "#fff" , gap:{md:3,xs:1,sm:2}, fontSize:{xs:'20px', sm:'30px'}}}
         display={"flex"}
         justifyContent={"center"}
         alignItems={"center"}
       
       >
-        00{isLast ? "" : <Box component="span">:</Box>}
+        {date.value}{isLast ? "" : <Box component="span">:</Box>}
       </Typography>
 
-      <Typography variant="body1"  component="p" p={1} sx={{ color: "#fff", }}>
-        {date}
+      <Typography variant="body1"  component="p" p={1} sx={{ color: "#fff",fontSize:{xs:'12px', sm:'20px'},pt:{xs:"12px",sm:"20px"} }}>
+        {date.name}
       </Typography>
     </Box>
   );
