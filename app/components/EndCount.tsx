e6376078f6c75d88f1d8d2e57687a311a4c649f8
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-06 17:55:43 +0330

    add(EndMessage): create component

---
+++ app/components/EndCount.tsx
@@ -0,0 +1,13 @@
+
+import { Typography }  from "@mui/material";
+
+
+export default function EndCount() {
+  return (
+    <Typography  color={"#EF4F4F"} px={6} sx={{fontSize:{xs:'30px', sm:'50px'}}} >
+
+        It's time
+            
+        </Typography>
+  )
+}
