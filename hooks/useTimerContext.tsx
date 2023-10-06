869cf42888dd14847ebbdc871a58ca08f9de7d67
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-06 15:49:47 +0330

    add(hooks): create usetimercontext

--- hooks/useTimerContext.tsx
+++ hooks/useTimerContext.tsx
@@ -1,6 +1,7 @@
+"use client"
 import {useContext} from 'react'
 import {TimerContext } from '../context/CounterContext'
 
-export const useLoginContext = () => {
+export const useTimerContext = () => {
   return useContext(TimerContext)
 }
