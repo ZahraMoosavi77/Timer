4409c6893dfe3678a80e6b8ff7509a5483cd3107
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-06 19:35:50 +0330

    fix(home): change margin

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-"use client";
-import { Grid, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import ToggleButtons from "./components/ToggleButtons";
 import BasicDateTimePicker from "./components/BasicDateTimePicker";
 import Button from "./components/Button";
 import Numbers from "./components/Numbers";
 import { TimerProvider } from "../context/CounterContext";
-import { useTimerContext } from "../hooks/useTimerContext";
-import { useEffect } from "react";
 
 export default function page() {
-  const { isEnd } = useTimerContext();
-  // useEffect(() => {
-  //   console.log(isEnd);
-  // }, [isEnd]);
-
   return (
     <TimerProvider>
       <Grid
@@ -29,8 +21,7 @@ export default function page() {
           direction="column"
           justifyContent="center"
           alignItems="center"
-          
-          sx={{ flexDirection: { sm: "row" }, gap:{xs:2,sm:0} }}
+          sx={{ flexDirection: { sm: "row" }, gap: { xs: 2, sm: 0 } }}
         >
           <Grid item xs={12} sm={6} md={4}>
             <BasicDateTimePicker />
@@ -41,11 +32,9 @@ export default function page() {
           </Grid>
         </Grid>
 
-      
-          <Grid item mt={20} xs={12}>
-            <Numbers />
-          </Grid>
-       
+        <Grid item mt={20} xs={12}>
+          <Numbers />
+        </Grid>
 
         <Grid item>
           <Button />
