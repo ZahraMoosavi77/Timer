5dafed046b7b5ab5503cefa8897f7cff4c91532e
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-06 16:39:06 +0330

    add(index.js): add dataArray

---
+++ constant/index.js
@@ -0,0 +1,10 @@
+const dateArray = [
+  { id: 1, name: "Year", value: years },
+  { id: 2, name: "Mounth", value: months },
+  { id: 3, name: "Days", value: days },
+  { id: 4, name: "Hours", value: hours },
+  { id: 5, name: "Minutes", value: minutes },
+  { id: 7, name: "Seconds", value: seconds },
+];
+
+export { dateArray };
