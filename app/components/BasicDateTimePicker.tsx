6653e0e1c3b793da545c8bb6c174e357f4435ef1
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-06 15:42:09 +0330

    fix(timepicker):fix borderradius

--- app/components/BasicDateTimePicker.tsx
+++ app/components/BasicDateTimePicker.tsx
@@ -6,18 +6,23 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import {useTimerContext} from "../../hooks/useTimerContext"
 
 export default function DateTimePickerValue() {
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17T15:30'));
+  // const [value, setValue] = React.useState<Dayjs | null>(dayjs('2023-05-10T15:30'));
+  const {value, setValue} = useTimerContext()
+
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DateTimePicker', 'DateTimePicker']}>
+    <LocalizationProvider dateAdapter={AdapterDayjs} >
+      <DemoContainer components={['DateTimePicker', 'DateTimePicker']} sx={{py:'0',borderRadius:'4px', border:'none', outline:'none'}}>
         <DateTimePicker
           
           value={value}
           onChange={(newValue) => setValue(newValue)}
-          sx={{color:'blue', width:'80px', bgcolor:'white',overflow:'hidden',  outline:'none'
+          autoFocus={false}
+          disablePast={true}
+          sx={{color:'red', width:'100px', bgcolor:'white',borderRadius:'4px', border:'none', outline:'none'
          
           }}
           
