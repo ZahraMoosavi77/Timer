130686639227214cdc9d884f052b520a0d2e7817
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-10-07 07:54:48 +0330

    feat(choosetimemessage): create choose correct time component

---
+++ app/components/ChooseTimeMessage.tsx
@@ -0,0 +1,11 @@
+import { Typography }  from "@mui/material";
+
+export default function ChooseTimeMessage() {
+  return (
+    <Typography  color={"#FC8210"} px={4} sx={{fontSize:{xs:'20px', sm:'40px'}}} >
+
+       Please Choose Correct Time
+            
+        </Typography>
+  )
+}
